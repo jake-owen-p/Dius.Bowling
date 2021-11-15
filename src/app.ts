9c35cb072c4192332cd2bddb6d0a6c92d1a8f3d8
Jake Owen <jakeowen.ex@gmail.com>
2021-11-15 13:30:45 +1100

    Refactor/ move score calculation to score() instead of calculing on the fly in roll()

--- src/app.ts
+++ src/app.ts
@@ -5,28 +5,24 @@ export class BowlingGame {
     private frames: number[][] = [[]];
 
     roll(score: number) {
-        const latestFrame = this.getLatestFrame();
-        const isSpare = latestFrame[0] + latestFrame[1] === MAX_BOWL;
-        if (isSpare) {
-            this.totalScore += score * 2;
-        } else {
-            this.totalScore += score;
-        }
-        this.addRollToFrame(score);
-    }
-
-    addRollToFrame = (score: number) => {
-        const latestFrame = this.getLatestFrame();
+        const latestFrame = this.frames[this.frames.length-1];
         const isNewFrame = latestFrame.length === 2 || latestFrame[0] === MAX_BOWL;
         isNewFrame
             ? this.frames.push([score])
             : this.frames[this.frames.length - 1].push(score);
     }
 
-    getLatestFrame() {
-        return this.frames[this.frames.length-1];
-    }
     score() {
+        for (let i = 0; i<this.frames.length; i++) {
+            const totalFrameScore = this.frames[i][0] + this.frames[i][1];
+            const isSpare = totalFrameScore === MAX_BOWL;
+            if (isSpare) {
+                const nextRoll = this.frames[i+1][0];
+                this.totalScore += totalFrameScore + nextRoll;
+            } else {
+                this.totalScore += totalFrameScore;
+            }
+        }
         return this.totalScore;
     }
 }
