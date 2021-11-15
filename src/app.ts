a84ed31748021d73fe4fa6190d6bda9975ed36f2
Jake Owen <jakeowen.ex@gmail.com>
2021-11-15 12:42:59 +1100

    Frame logic in bowling game to store rolls & spare functionality

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
+const MAX_BOWL = 10;
+
 export class BowlingGame {
     private totalScore = 0;
+    private frames: number[][] = [[]];
 
     roll(score: number) {
-        this.totalScore += score;
+        const latestFrame = this.getLatestFrame();
+        const isSpare = latestFrame[0] + latestFrame[1] === MAX_BOWL;
+        if (isSpare) {
+            this.totalScore += score * 2;
+        } else {
+            this.totalScore += score;
+        }
+        this.addRollToFrame(score);
+    }
+
+    addRollToFrame = (score: number) => {
+        const latestFrame = this.getLatestFrame();
+        const isNewFrame = latestFrame.length === 2 || latestFrame[0] === MAX_BOWL;
+        isNewFrame
+            ? this.frames.push([score])
+            : this.frames[this.frames.length - 1].push(score);
+    }
+
+    getLatestFrame() {
+        return this.frames[this.frames.length-1];
     }
     score() {
         return this.totalScore;
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -6,27 +6,47 @@ const rollFullGame = (bowlingGame: BowlingGame, score: number) => {
         bowlingGame.roll(score);
     }
 }
+
 describe('GIVEN a bowling game with a valid set of rolls', () => {
+    let bowlingGame: BowlingGame;
+    beforeEach(() => {
+        bowlingGame = new BowlingGame();
+    })
     describe('WHEN all rolls are 0', () => {
         it('THEN the score is 0', () => {
-            const bowlingGame = new BowlingGame();
             rollFullGame(bowlingGame, 0);
             expect(bowlingGame.score()).toEqual(0);
         })
     })
     describe('WHEN all rolls are 1', () => {
         it('THEN the score is 20', () => {
-            const bowlingGame = new BowlingGame();
             rollFullGame(bowlingGame, 1);
             expect(bowlingGame.score()).toEqual(20);
         })
     })
-    describe('WHEN there are two rolls, 4 and 4', () => {
+    describe('WHEN there are two rolls [4,4]', () => {
         it('THEN the score is 8', () => {
-            const bowlingGame = new BowlingGame();
             bowlingGame.roll(4);
             bowlingGame.roll(4);
             expect(bowlingGame.score()).toEqual(8);
+        })
+    })
+    describe('WHEN there are four rolls [4,6,5,0] AND the first two count as a spare', () => {
+        it('THEN the score is 20', () => {
+            const rolls = [4,6,5,0];
+            for (const roll of rolls) {
+                bowlingGame.roll(roll);
+            }
+            expect(bowlingGame.score()).toEqual(20);
+        })
+    })
+    describe('WHEN there are four rolls [4,6,5,5,5,4] AND the first two count as a spare', () => {
+        it('THEN the score is 20', () => {
+            const rolls = [4,6,5,5,5,4];
+            for (const roll of rolls) {
+                bowlingGame.roll(roll);
+            }
+            expect(bowlingGame.score()).toEqual(39);
         })
     })
 })
