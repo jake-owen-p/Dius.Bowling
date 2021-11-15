30f180210ca33dcdfd253e6937d8da9f248e27e2
Jake Owen <jakeowen.ex@gmail.com>
2021-11-15 22:46:17 +1100

    Move test utils within describe to not have to pass game in each call

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,30 +1,30 @@
 import {BowlingGame} from "../src/app";
 
-const rollSome = (bowlingGame: BowlingGame, score: number, rolls: number) => {
-    for (let i = 0; i<rolls; i++) {
-        bowlingGame.roll(score);
-    }
-}
-const rollSelection = (bowlingGame: BowlingGame, rolls: number[]) => {
-    for (const roll of rolls) {
-        bowlingGame.roll(roll);
-    }
-}
 describe('GIVEN a bowling game with a valid set of rolls', () => {
     let bowlingGame: BowlingGame;
     beforeEach(() => {
         bowlingGame = new BowlingGame();
     })
+    const rollSome = (score: number, rolls: number) => {
+        for (let i = 0; i<rolls; i++) {
+            bowlingGame.roll(score);
+        }
+    }
+    const rollSelection = (rolls: number[]) => {
+        for (const roll of rolls) {
+            bowlingGame.roll(roll);
+        }
+    }
     describe('WHEN all roles are the same', () => {
         describe('AND all rolls are 0', () => {
             it('THEN the score is 0', () => {
-                rollSome(bowlingGame, 0, 20);
+                rollSome(0, 20);
                 expect(bowlingGame.score()).toEqual(0);
             })
         })
         describe('AND all rolls are 1', () => {
             it('THEN the score is 20', () => {
-                rollSome(bowlingGame, 1, 20);
+                rollSome(1, 20);
                 expect(bowlingGame.score()).toEqual(20);
             })
         })
@@ -32,7 +32,7 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
             it('THEN the score is 8', () => {
                 bowlingGame.roll(4);
                 bowlingGame.roll(4);
-                rollSome(bowlingGame, 0, 18);
+                rollSome(0, 18);
                 expect(bowlingGame.score()).toEqual(8);
             })
         })
@@ -41,23 +41,23 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
         describe('AND there are four rolls [4,6,5,0] AND the first frame is a spare', () => {
             it('THEN the score is 20', () => {
                 const rolls = [4,6,5,0];
-                rollSelection(bowlingGame, rolls);
-                rollSome(bowlingGame, 0, 16);
+                rollSelection(rolls);
+                rollSome(0, 16);
                 expect(bowlingGame.score()).toEqual(20);
             })
         })
         describe('AND there are four rolls [4,6,5,5,5,4] AND the first two frames are spares', () => {
             it('THEN the score is 39', () => {
                 const rolls = [4,6,5,5,5,4];
-                rollSelection(bowlingGame, rolls);
-                rollSome(bowlingGame, 0, 14);
+                rollSelection(rolls);
+                rollSome(0, 14);
                 expect(bowlingGame.score()).toEqual(39);
             })
         })
         describe('AND the game is all spares', () => {
             it('THEN the score is 300', () => {
                 const rolls = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5];
-                rollSelection(bowlingGame, rolls);
+                rollSelection(rolls);
                 expect(bowlingGame.score()).toEqual(150);
             })
         })
@@ -66,15 +66,15 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
         describe('AND there are three rolls [10,5,4] AND the first frame is a strike', () => {
             it('THEN the score is 28', () => {
                 const rolls = [10,5,4];
-                rollSelection(bowlingGame, rolls);
-                rollSome(bowlingGame, 0, 16);
+                rollSelection(rolls);
+                rollSome(0, 16);
                 expect(bowlingGame.score()).toEqual(28);
             })
         })
         describe('AND the game is all strikes', () => {
             it('THEN the score is 300', () => {
                 const rolls = [10,10,10,10,10,10,10,10,10,10,10,10];
-                rollSelection(bowlingGame, rolls);
+                rollSelection(rolls);
                 expect(bowlingGame.score()).toEqual(300);
             })
         })
@@ -83,14 +83,14 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
         describe('AND the rolls are [10,10,2,4,6,2,5,5,4,4,10,4,3,10,2,2]', () => {
             it('THEN the score is 116', () => {
                 const rolls = [10,10,2,4,6,2,5,5,4,4,10,4,3,10,2,2];
-                rollSelection(bowlingGame, rolls);
+                rollSelection(rolls);
                 expect(bowlingGame.score()).toEqual(116);
             })
         })
         describe('AND the rolls are [8,2,4,5,4,6,10,10,7,3,3,3,2,5,7,3,10,10,10]', () => {
             it('THEN the score is 166', () => {
                 const rolls = [8,2,4,5,4,6,10,10,7,3,3,3,2,5,7,3,10,10,10];
-                rollSelection(bowlingGame, rolls);
+                rollSelection(rolls);
                 expect(bowlingGame.score()).toEqual(166);
             })
         })
