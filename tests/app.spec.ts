1cc4a97bfccc75c72c8fc44461a2e5b2768bfa17
Jake Owen <jakeowen.ex@gmail.com>
2021-11-15 22:43:52 +1100

    Add test cases for game of spares + mixed games

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -54,6 +54,13 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
                 expect(bowlingGame.score()).toEqual(39);
             })
         })
+        describe('AND the game is all spares', () => {
+            it('THEN the score is 300', () => {
+                const rolls = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5];
+                rollSelection(bowlingGame, rolls);
+                expect(bowlingGame.score()).toEqual(150);
+            })
+        })
     })
     describe('WHEN considering only a strike', () => {
         describe('AND there are three rolls [10,5,4] AND the first frame is a strike', () => {
@@ -65,10 +72,26 @@ describe('GIVEN a bowling game with a valid set of rolls', () => {
             })
         })
         describe('AND the game is all strikes', () => {
-            it('THEN the score is 28', () => {
+            it('THEN the score is 300', () => {
                 const rolls = [10,10,10,10,10,10,10,10,10,10,10,10];
                 rollSelection(bowlingGame, rolls);
                 expect(bowlingGame.score()).toEqual(300);
+            })
+        })
+    })
+    describe('WHEN considering game consisting of spares, strikes and regular scores', () => {
+        describe('AND the rolls are [10,10,2,4,6,2,5,5,4,4,10,4,3,10,2,2]', () => {
+            it('THEN the score is 116', () => {
+                const rolls = [10,10,2,4,6,2,5,5,4,4,10,4,3,10,2,2];
+                rollSelection(bowlingGame, rolls);
+                expect(bowlingGame.score()).toEqual(116);
+            })
+        })
+        describe('AND the rolls are [8,2,4,5,4,6,10,10,7,3,3,3,2,5,7,3,10,10,10]', () => {
+            it('THEN the score is 166', () => {
+                const rolls = [8,2,4,5,4,6,10,10,7,3,3,3,2,5,7,3,10,10,10];
+                rollSelection(bowlingGame, rolls);
+                expect(bowlingGame.score()).toEqual(166);
             })
         })
     })
